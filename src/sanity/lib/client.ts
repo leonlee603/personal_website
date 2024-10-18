d55824eec44e1ee4dfd489b6596b31949c71874c
Leon <leepingho603@gmail.com>
2024-10-18 11:45:06 +0800

    Update - Typegen setup

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -2,6 +2,12 @@ import { createClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId } from '../env'
 
+/**
+ * Sanity Client is a JavaScript library commonly used to interact with Sanity projects. 
+ * Its most basic function is querying content, but once authenticated with a token, 
+ * it can interact with almost every part of a Sanity project.
+ */
+
 export const client = createClient({
   projectId,
   dataset,
