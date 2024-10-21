7cb450a33b64e163356e310ae8d93d9d14f8af8b
Leon <leepingho603@gmail.com>
2024-10-21 15:37:52 +0800

    Update - Add visual editing feature

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import '@/app/globals.css';
 import { Header } from '@/components/Header'
+import { VisualEditing } from 'next-sanity'
+import { draftMode } from 'next/headers'
 
 export const metadata: Metadata = {
   title: 'Leon',
@@ -15,8 +17,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-white min-h-screen">
+      {draftMode().isEnabled && (
+          <a
+            className="fixed right-0 bottom-0 bg-blue-500 text-white p-4 m-4"
+            href="/api/draft-mode/disable"
+          >
+            Disable preview mode
+          </a>
+        )}
         <Header />
         {children}
+        {draftMode().isEnabled && <VisualEditing />}
       </body>
     </html>
   );
--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,12 @@
-import { createClient, type QueryParams } from 'next-sanity'
+import { createClient, QueryOptions, type QueryParams } from "next-sanity";
+import { draftMode } from "next/headers";
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId } from "../env";
+import { token } from "./token";
 
 /**
- * Sanity Client is a JavaScript library commonly used to interact with Sanity projects. 
- * Its most basic function is querying content, but once authenticated with a token, 
+ * Sanity Client is a JavaScript library commonly used to interact with Sanity projects.
+ * Its most basic function is querying content, but once authenticated with a token,
  * it can interact with almost every part of a Sanity project.
  */
 
@@ -13,7 +15,11 @@ export const client = createClient({
   dataset,
   apiVersion,
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  stega: {
+    enabled: process.env.NEXT_PUBLIC_VERCEL_ENV === "preview",
+    studioUrl: "/studio",
+  },
+});
 
 export async function sanityFetch<const QueryString extends string>({
   query,
@@ -21,15 +27,35 @@ export async function sanityFetch<const QueryString extends string>({
   revalidate = 60, // default revalidation time in seconds
   tags = [],
 }: {
-  query: QueryString
-  params?: QueryParams
-  revalidate?: number | false
-  tags?: string[]
+  query: QueryString;
+  params?: QueryParams;
+  revalidate?: number | false;
+  tags?: string[];
 }) {
+  const isDraftMode = draftMode().isEnabled;
+
+  if (isDraftMode && !token) {
+    throw new Error("Missing environment variable SANITY_API_READ_TOKEN");
+  }
+
+  const queryOptions: QueryOptions = {};
+  let maybeRevalidate = revalidate;
+
+  if (isDraftMode) {
+    queryOptions.token = token
+    queryOptions.perspective = 'previewDrafts'
+    queryOptions.stega = true
+
+    maybeRevalidate = 0 // Do not cache in Draft Mode
+  } else if (tags.length) {
+    maybeRevalidate = false // Cache indefinitely if tags supplied
+  }
+
   return client.fetch(query, params, {
+    ...queryOptions,
     next: {
-      revalidate: tags.length ? false : revalidate, // for simple, time-based revalidation
+      revalidate: maybeRevalidate,
       tags, // for tag-based revalidation
     },
-  })
+  });
 }
