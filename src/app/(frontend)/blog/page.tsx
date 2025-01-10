96f67c5f6a64dc8e01887685afec5eb8a1a0501f
Leon <leepingho603@gmail.com>
2025-01-10 16:53:14 +0800

    Update - BlogCategories component refactoring

--- src/app/(frontend)/blog/page.tsx
+++ src/app/(frontend)/blog/page.tsx
@@ -3,6 +3,7 @@ import { CATEGORIES_QUERY, POSTS_IN_CATEGORY_RANGE_QUERY, POSTS_COUNT, POSTS_IN_
 import { PostCard } from "@/components/PostCard";
 import { Title } from "@/components/Title";
 import Link from "next/link";
+import BlogCategories from "@/components/BlogCategories";
 
 interface SearchParams {
   [key: string]: string | string[] | undefined;
@@ -26,25 +27,7 @@ export default async function Page({ searchParams }: { searchParams: Promise<Sea
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
       <Title>Blog</Title>
       <div>Personal articles about life, hobbies, photography, and everything else.</div>
-      <div>
-        <Link href={`/blog`}>
-          <span className="bg-cyan-50 rounded-full px-2 py-1 leading-none whitespace-nowrap text-sm font-semibold text-cyan-700">
-            All categories
-          </span>
-        </Link>
-        {categories.map((category) => {
-          return (
-            <Link
-              key={category._id}
-              href={`/blog?category=${category.slug!.current}`}
-            >
-              <span className="bg-cyan-50 rounded-full px-2 py-1 leading-none whitespace-nowrap text-sm font-semibold text-cyan-700">
-                {category.title}
-              </span>
-            </Link>
-          );
-        })}
-      </div>
+      <BlogCategories categories={categories} />
       <div className="flex flex-col gap-24 py-12">
         {posts.map((post) => (
           <PostCard key={post._id} {...post} />
---
+++ src/components/BlogCategories.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import type { CATEGORIES_QUERYResult } from "@/sanity/types";
+
+export default function BlogCategories({
+  categories,
+}: {
+  categories: CATEGORIES_QUERYResult;
+}) {
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", columnGap: '1rem', rowGap: '1rem' }}>
+      <Link href={`/blog`}>
+        <span className="bg-cyan-50 rounded-full px-2 py-1 leading-none whitespace-nowrap text-sm font-semibold text-cyan-700">
+          All categories
+        </span>
+      </Link>
+      {categories.map((category) => {
+        return (
+          <Link
+            key={category._id}
+            href={`/blog?category=${category.slug!.current}`}
+          >
+            <span className="bg-cyan-50 rounded-full px-2 py-1 leading-none whitespace-nowrap text-sm font-semibold text-cyan-700">
+              {category.title}
+            </span>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
