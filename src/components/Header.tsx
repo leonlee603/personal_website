f4860679c14aa054641dff11c4077fe6e398f63c
Leon Lee <leepingho603@gmail.com>
2025-01-12 11:24:55 +0800

    Update - Add Theming & dark mode & mode toggle btn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,34 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { ThemeToggleBtn } from './ModeToggleBtn';
+import { Button } from './ui/button';
 
 export function Header() {
   return (
-    <div className="from-pink-50 to-white bg-gradient-to-b p-6">
-      <header className="bg-white/80 shadow-md flex items-center justify-between p-6 rounded-lg container mx-auto shadow-pink-50">
-        <Link
-          className="text-pink-700 md:text-xl font-bold tracking-tight"
-          href="/"
-        >
+    <div className="from-pink-50 to-white  p-6">
+      <header className="flex items-center justify-between p-6 rounded-lg container mx-auto shadow-pink-50">
+        <Link className="font-bold tracking-tight" href="/">
           Leon
         </Link>
-        <ul className="flex items-center gap-4 font-semibold text-slate-700">
-          <li>
-            <Link
-              className="hover:text-pink-500 transition-colors"
-              href="/blog"
-            >
-              Blog
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="hover:text-pink-500 transition-colors"
-              href="/studio"
-            >
-              Sanity Studio
-            </Link>
-          </li>
-        </ul>
+        <div className="flex flex-row gap-6">
+          <ul className="flex items-center gap-1 font-semibold text-slate-700">
+            <li>
+              <Button variant="ghost" asChild>
+                <Link className="" href="/blog">
+                  Blog
+                </Link>
+              </Button>
+            </li>
+            <li>
+              <Button variant="ghost" asChild>
+                <Link className="" href="/studio">
+                  Sanity Studio
+                </Link>
+              </Button>
+            </li>
+          </ul>
+          <ThemeToggleBtn />
+        </div>
       </header>
     </div>
-  )
+  );
 }
