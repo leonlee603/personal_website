90b6d6b78b7187af38947f7c5a6fcdf914927d05
Leon <leepingho603@gmail.com>
2025-01-13 18:11:59 +0800

    Update - Blog page & pagination

--- src/app/(frontend)/blog/page.tsx
+++ src/app/(frontend)/blog/page.tsx
@@ -1,26 +1,46 @@
 import { sanityFetch } from "@/sanity/lib/live";
-import { CATEGORIES_QUERY, POSTS_IN_CATEGORY_RANGE_QUERY, POSTS_COUNT, POSTS_IN_CATEGORY_COUNT, POSTS_RANGE_QUERY } from "@/sanity/lib/queries";
+import {
+  CATEGORIES_QUERY,
+  POSTS_IN_CATEGORY_RANGE_QUERY,
+  POSTS_COUNT,
+  POSTS_IN_CATEGORY_COUNT,
+  POSTS_RANGE_QUERY,
+} from "@/sanity/lib/queries";
 import { PostCard } from "@/components/PostCard";
 import { Title } from "@/components/Title";
-import Link from "next/link";
 import BlogCategories from "@/components/BlogCategories";
+import { BlogPagination } from "@/components/BlogPagination";
 
 interface SearchParams {
   [key: string]: string | string[] | undefined;
 }
 
-export default async function Page({ searchParams }: { searchParams: Promise<SearchParams> }) {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) {
   // get ready for pagination
   const params = await searchParams;
   const currentPage = parseInt(params.page as string) || 1;
   const postsPerPage = 12;
   const start = (currentPage - 1) * postsPerPage;
   const end = start + postsPerPage;
-  const {data: totalPosts} = params.category ? await sanityFetch({ query: POSTS_IN_CATEGORY_COUNT, params: {category:params.category } }) : await sanityFetch({query:POSTS_COUNT});
+  const { data: totalPosts } = params.category
+    ? await sanityFetch({
+        query: POSTS_IN_CATEGORY_COUNT,
+        params: { category: params.category },
+      })
+    : await sanityFetch({ query: POSTS_COUNT });
   const totalPages = Math.ceil(totalPosts / postsPerPage);
 
   // fetching data
-  const {data: posts} = params.category ? await sanityFetch({ query: POSTS_IN_CATEGORY_RANGE_QUERY, params: {category: params.category, start, end} }) : await sanityFetch({ query: POSTS_RANGE_QUERY, params: {start, end} });
+  const { data: posts } = params.category
+    ? await sanityFetch({
+        query: POSTS_IN_CATEGORY_RANGE_QUERY,
+        params: { category: params.category, start, end },
+      })
+    : await sanityFetch({ query: POSTS_RANGE_QUERY, params: { start, end } });
   const { data: categories } = await sanityFetch({ query: CATEGORIES_QUERY });
 
   return (
@@ -28,22 +48,26 @@ export default async function Page({ searchParams }: { searchParams: Promise<Sea
       <div className="container mx-auto max-w-1248">
         <div className="flex flex-col gap-3 py-12">
           <Title>Blog</Title>
-          <div>Personal articles about life, hobbies, photography, and everything else.</div>
+          <div>
+            Personal articles about life, hobbies, photography, and everything
+            else.
+          </div>
         </div>
-        <BlogCategories currentCategory={params.category} categories={categories} />
-        <div className="grid grid-cols-4 gap-6 py-12">
-          {posts.map((post) => (
-            <PostCard key={post._id} {...post} />
-          ))}
-        </div>
-        <div>
-            {currentPage > 1 && (
-              <Link href={params.category ? `?category=${params.category}&&page=${currentPage - 1}` : `?page=${currentPage - 1}`}>Previous</Link>
-            )}
-            
-            {currentPage < totalPages && (
-              <Link href={params.category ? `?category=${params.category}&&page=${currentPage + 1}` :`?page=${currentPage + 1}` }>Next</Link>
-            )}
+        <BlogCategories
+          currentCategory={params.category}
+          categories={categories}
+        />
+        <div className="mb-24 mt-12">
+          <div className="grid grid-cols-4 gap-6 pb-12">
+            {posts.map((post) => (
+              <PostCard key={post._id} {...post} />
+            ))}
+          </div>
+          <BlogPagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            params={params}
+          />
         </div>
       </div>
     </main>
