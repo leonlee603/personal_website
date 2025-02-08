59fb59ea0c1da5e744c09a54b35204f42987ec21
Leon Lee <leepingho603@gmail.com>
2025-02-08 16:14:27 +0800

    Update - Add dynamic metadata to dynamic route

--- src/app/(frontend)/blog/[slug]/page.tsx
+++ src/app/(frontend)/blog/[slug]/page.tsx
@@ -1,7 +1,19 @@
+import type { Metadata } from 'next'
 import { sanityFetch } from "@/sanity/lib/live";
 import { POST_QUERY } from "@/sanity/lib/queries";
 import { Post } from "@/components/Post";
 import { notFound } from "next/navigation";
+
+export async function generateMetadata({params} : {params: Promise<{ slug: string }>}):Promise<Metadata> {
+  const { data: post } = await sanityFetch({
+    query: POST_QUERY,
+    params: await params,
+  });
+
+  return {
+    title: post!.title
+  }
+}
 
 export default async function Page({
   params,
@@ -12,11 +24,11 @@ export default async function Page({
     query: POST_QUERY,
     params: await params,
   });
-
+  
   if (!post) {
     notFound();
   }
-
+  
   return (
     <div className="px-8">
       <main className="container mx-auto max-w-1248 pb-24 pt-12">
--- src/app/(frontend)/notes/[slug]/page.tsx
+++ src/app/(frontend)/notes/[slug]/page.tsx
@@ -1,7 +1,19 @@
+import type { Metadata } from 'next'
 import { sanityFetch } from "@/sanity/lib/live";
 import { NOTE_QUERY } from "@/sanity/lib/queries";
 import { Note } from "@/components/Note";
 import { notFound } from "next/navigation";
+
+export async function generateMetadata({params} : {params: Promise<{ slug: string }>}):Promise<Metadata> {
+  const { data: post } = await sanityFetch({
+    query: NOTE_QUERY,
+    params: await params,
+  });
+
+  return {
+    title: post!.title
+  }
+}
 
 export default async function Page({
   params,
