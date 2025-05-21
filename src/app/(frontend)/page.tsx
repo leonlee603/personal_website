40cc86497d7995bf9507ad139f1f20a72956687a
Leon <leepingho603@gmail.com>
2025-05-21 18:29:03 +0800

    Update - Home page - wip

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,19 +1,77 @@
 import Link from "next/link";
+import { FaMapMarkerAlt } from "react-icons/fa";
 import Title from "@/components/Title";
+import { Button } from "@/components/ui/button";
+import { Suspense } from "react";
+import NotesList from "@/components/NotesList";
+import BlogListHome from "@/components/BlogListHome";
 
 //TODO add description
-// export const metadata = {
-//   description: 'To be added.'
-// };
+export const metadata = {
+  description: "Developer, a personal website",
+};
 
 export default async function Page() {
   return (
-    <section className="container mx-auto max-w-1248">
-      <Title>Hey there! I&apos;m Leon 🌳</Title>
-      <p>I&apos;m a self-taught programmer passionate on learning new things, currently working on Next.js.</p>
-      <p>I enjoy weight-lifting, reading books and playing video games.</p>
-      <hr />
-      <Link href="/blog">Blog posts index &rarr;</Link>
+    <section className="px-8">
+      <div className="container mx-auto max-w-1248">
+        <div className="flex flex-col gap-3 py-12">
+          <Title>Hey there! I&apos;m Leon 🌳</Title>
+          <div className="mb-4 flex items-center gap-2">
+            <FaMapMarkerAlt className="text-primary" />
+            <div className="text-muted-foreground">Hong Kong</div>
+          </div>
+          <p>
+            I&apos;m a self-taught developer passionate on learning new things,
+            currently working on Next.js.
+          </p>
+          <p>I enjoy weight-lifting, reading books and playing video games.</p>
+        </div>
+        <Button
+          asChild
+          variant="outline"
+          size="lg"
+          className="mb-12 inline-flex"
+        >
+          <Link className="hover:text-primary" href="/me">
+            More about me
+          </Link>
+        </Button>
+        {/* <hr /> */}
+        <div className="flex flex-col gap-3 py-12">
+          <div className="flex items-baseline gap-4">
+            <h2 className="text-pretty text-xl font-semibold md:text-2xl lg:text-4xl">
+              Blog
+            </h2>
+            <Link href="/blog">See all</Link>
+          </div>
+          <div className="mb-12">
+            Articles about personal life, hobbies, photography, and everything
+            else.
+          </div>
+          <Suspense>
+            <BlogListHome category="" currentPage={1} postsPerPage={4} />
+          </Suspense>
+        </div>
+        <div className="flex flex-col gap-3 py-12">
+          <div className="flex items-baseline gap-4">
+            <h2 className="text-pretty text-xl font-semibold md:text-2xl lg:text-4xl">
+              Notes
+            </h2>
+            <Link href="/notes">See all</Link>
+          </div>
+          <div className="mb-12">
+            Notes, references, and tutorials on programming, web development.
+          </div>
+          <Suspense>
+            <NotesList
+              topic=""
+              currentPage={1}
+              notesPerPage={5}
+            />
+          </Suspense>
+        </div>
+      </div>
     </section>
   );
 }
---
+++ src/components/BlogListHome.tsx
@@ -0,0 +1,59 @@
+import { notFound } from "next/navigation";
+import { sanityFetch } from "@/sanity/lib/live";
+import {
+  POSTS_IN_CATEGORY_RANGE_QUERY,
+  POSTS_RANGE_QUERY,
+} from "@/sanity/lib/queries";
+import Link from "next/link";
+import { Categories } from "./Categories";
+import { PublishedAt } from "./PublishedAt";
+
+export default async function BlogListHome({
+  category,
+  currentPage,
+  postsPerPage,
+}: {
+  category: string;
+  currentPage: number;
+  postsPerPage: number;
+}) {
+  const start = (currentPage - 1) * postsPerPage;
+  const end = start + postsPerPage;
+
+  const { data: posts } = category
+    ? await sanityFetch({
+        query: POSTS_IN_CATEGORY_RANGE_QUERY,
+        params: { category: category, start, end },
+      })
+    : await sanityFetch({ query: POSTS_RANGE_QUERY, params: { start, end } });
+
+  if (!posts || !posts.length) {
+    notFound();
+  }
+
+  return (
+    <div className="flex flex-col border-t-2 border-dotted pb-12">
+      {posts.map((post) => {
+        const { title, publishedAt, categories } = post;
+        return (
+          <Link key={post._id}className="" href={`/notes/${post.slug!.current}`}>
+            <div className="flex flex-row justify-between gap-6 overflow-hidden border-b-2 border-dotted py-5">
+              <div className="max-w-70 text-lg font-semibold leading-none tracking-tight">
+                {title}
+              </div>
+              <div className="flex flex-row items-center gap-4">
+                <div className="flex flex-wrap items-center gap-2">
+                  <Categories categories={categories} />
+                </div>
+                <PublishedAt
+                  publishedAt={publishedAt}
+                  className="mb-0 text-nowrap"
+                />
+              </div>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
